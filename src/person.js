6f4a4b06ec5e52e48fdf6af1343721c54c092645
Mehmet Aydoğdu <140202062@kocaeli.edu.tr>
2017-09-16 19:37:19 +0300

    my resume
    https://github.com/salomonelli/best-resume-ever

---
+++ src/person.js
@@ -0,0 +1,103 @@
+/* eslint-disable */
+export const PERSON = {
+  name: {
+    first: 'Mehmet',
+    middle: '',
+    last: 'Aydoğdu',
+  },
+  position: 'Software Developer',
+  birth: {
+    year: 1995,
+    location: 'Kocaeli / Turkey'
+  },
+  experience: [{
+      company: 'Dreamious. Inc,',
+      position: 'CEO',
+      timeperiod: 'since January 2017',
+      description: 'Performing all processes when developing a startup'
+    },
+    {
+      company: 'Birkaç iyi adam',
+      position: 'Product Design',
+      timeperiod: 'February 2015',
+      description: 'Design & Development Support for Startups'
+    },         
+    {
+      company: 'USTAMDAN',
+      position: 'Software Development',
+      timeperiod: 'February 2014 - February 2016',
+      description: 'Developing a full featured e-commerce web app'
+    },
+    {
+      company: 'Monster Clouds',
+      position: 'Founder & Adversiting Consultant',
+      timeperiod: 'May 2013 - October 2013',
+      description: 'Administrating Linux Servers, Website Design & Development, SEO & Search Engine Marketing'
+    },
+    {
+      company: 'M2 Game Studio',
+      position: 'Founder & Game Developer',
+      timeperiod: 'March 2011 - August 2011',
+      description: '3D MMORPG Game with Python /br Load balancing for FreeBSD game core servers and MySQL database servers /br FreeBSD hardening and researching Firewall Rules aganist attacks'
+    }
+  ],
+  education: [{
+      degree: 'Intern',
+      timeperiod: 'December 2015 - May 2016',
+      description: 'Founder Instutue - fi.co'
+    },
+    {
+      degree: "Bachelor's Degree",
+      timeperiod: '2014 - 2018',
+      description: 'Kocaeli University, Computer Science'
+    }
+  ],
+  // skill level goes 0 to 100
+  skills: [{
+      name: 'UI Design',
+      level: '100'
+    },
+    {
+      name: 'UI Prototyping',
+      level: '100'
+    },
+    {
+      name: 'Frond-end Development',
+      level: '100'
+    },
+    {
+      name: 'JavaScript',
+      level: '100'
+    },
+    {
+      name: 'Ruby on Rails',
+      level: '100'
+    },
+    {
+      name: 'Open Source Platforms',
+      level: '90'
+    },
+    {
+      name: 'Linux Server',
+      level: '80'
+    },
+    {
+      name: 'Growth Hacking',
+      level: '85'
+    },
+    {
+      name: 'Python',
+      level: '75'
+    }
+  ],
+  skillDescription: 'Also proficient in Adobe Photoshop, Illustrator and Sketch, grew up bilingual (English and Turkish).',
+  contact: {
+    email: 'mhmt.dp@gmail.com',
+    phone: '90 531 329 31 52',
+    street: 'Piotr Wilcynskl 41030',
+    city: 'Kocaeli',
+    website: 'about.me/mehmetaydogdu',
+    github: 'cooldesigns'
+  },
+  lang: 'en' // en, de, fr
+};
